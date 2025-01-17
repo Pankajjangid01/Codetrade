4cd022b5ea4c0c930e8f2e54835169e4d22756eb
Pankajjangid01 <pankajjangir1034@gmail.com>
2025-01-17 16:28:25 +0530

    parsed the data without using json library

--- api_integration/whether_details.py
+++ api_integration/whether_details.py
@@ -1,5 +1,5 @@
 """ Imported json and request library """
-import json
+# import json
 import requests
 
 class Whether:
@@ -18,24 +18,26 @@ class Whether:
         self.temperature=None
         self.wind_speed=None
         self.api_response=None
-        self.parse_data=None
+        self.parse_data={}
         self.pressure=None
 
     def find_whether(self):
         """
-            Description: Method to find the whether of the particular city and extract the humidity, temperature, pressure, wind_speed
+            Description: Method to find the whether of the particular cit+y and extract the humidity, temperature, pressure, wind_speed
         """
         try:
             print("Fetching data...")
+            # import pdb 
+            # pdb.set_trace()
             self.api_response = requests.get(self.complete_url).text  # pull the data from api
-            self.parse_data = json.loads(self.api_response)  # parse the data into json format
+            # self.parse_data = json.loads(self.api_response)  # parse the data
+            self.parse_data = eval(self.api_response)  # parse the data 
             self.humidity = self.parse_data['main']['humidity']  # extract the humitdity
             self.temperature = self.parse_data['main']['temp']  # extract the temperature
             self.pressure = self.parse_data['main']['pressure']  # extract the pressure
             self.wind_speed = self.parse_data['wind']['speed']  # extract the wind speed
             print(f"Whether Details of {self.city}:\nHumidity: {self.humidity}, Temprature: {self.temperature}, Pressure: {self.pressure}, Wind Speed:{self.wind_speed}")  # printing the extracted details
             self.store_whether_details()  # call the store whether method to store whether details in csv file
-
         except KeyError as key:
             print(f"Error fetching data:{key}\n{self.api_response}")
 
@@ -47,7 +49,7 @@ class Whether:
             print("saving whether details...")
             with open('whether.csv','a') as whether_file:
                 if whether_file.tell() == 0:  # check if it is header or not, if it is header then write header in the file
-                    whether_file.write("City Name,Humidity,Temprature,Pressure,Wind Speed\n")
+                    whether_file.write("City-Name,Humidity,Temprature,Pressure,Wind Speed\n")
                 whether_file.write(f"{self.city},{self.humidity},{self.temperature},{self.pressure},{self.wind_speed}\n")  #write details in each row 
                 print("Whether Details saved successfully...")
         except IOError:
