3a4d7085b1dd64e6659ba3bd8a8ef7946fb743a4
Pankajjangid01 <pankajjangir1034@gmail.com>
2025-01-20 15:04:37 +0530

    updated task7

--- python_tasks/task1.py
+++ python_tasks/task1.py
@@ -8,23 +8,15 @@ def get_list_element(my_list):
         if item <5:
             print(my_list[item], end = ' ')
 
-    # return my_list[:5]
-
 def get_tuple_element(my_tuple):
     """return the first five element of tuple"""
     for item in range(len(my_tuple)):
         if item <5:
             print(my_tuple[item], end = ' ')
-    # return my_tuple[:5]
-
-def get_set_elements(my_set):
-    """return the first five element of set"""
-    for element in my_set:
-        if element < 6:
-            print(element, end=" ")  
 
 def get_last_character(string):
     return string[:5]
+
 my_list = [1,2,3,4,8,5,6,2,7]
 print("First five element of the list:")
 get_list_element(my_list)
@@ -32,10 +24,6 @@ get_list_element(my_list)
 my_tuple = (8,2,5,3,4,6)
 print("\nFirst five element of the Tuple:")
 get_tuple_element(my_tuple)
-
-my_set = {9,4,5,6,2,0,4,3}
-print("\nFirst five element of the Set:")
-get_set_elements(my_set)
 
 print("\nFirst five element of the String:")
 string = "Coding"
--- python_tasks/task7.py
+++ python_tasks/task7.py
@@ -2,5 +2,9 @@
     Description:Generate a dictionary {1:1,2:1,3:1,4:1,...,10:1} in one line using dictionary's
     method.
 """
-my_dict = dict({1:1,2:1,3:1,4:1,5:1,6:1,7:1,8:1,9:1,10:1})  #  creating the dictionary 
-print(my_dict)
+
+print("Enter a list, separated by spaces:")
+user_input = input()
+list = list(map(int, user_input.split()))
+new={i+1:x for i,x in enumerate(list)}
+print(new)
